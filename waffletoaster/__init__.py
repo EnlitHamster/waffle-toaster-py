385d0b9a33a9ca887068330cfdf7bfcf17d8a7d3
Sandro Massa <sandro2398@hotmail.it>
2021-04-14 14:36:31 +0200

    Moved configuration to instance config.py file

--- waffletoaster/__init__.py
+++ waffletoaster/__init__.py
@@ -7,16 +7,6 @@ from flask_mongoengine import MongoEngine
 def create_app(test_config=None):
     # create and cofigure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        MONGODB_SETTINGS={
-            'db': 'waffletoaster',
-            'host': 'localhost',
-            'port': 27017
-        },
-    )
-
-    db = MongoEngine(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,6 +14,8 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+    
+    db = MongoEngine(app)
 
     # ensure the instance folder exists
     try:
